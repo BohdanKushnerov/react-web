1798422aa44d93fb6874871003d7b14145a5b436
BohdanKushnerov <bohdan.kushnerov@gmail.com>
2024-06-26 09:43:01 +0000

    Deploying to gh-pages from @ BohdanKushnerov/react-web@aa6f44d3b4a4e77dc5e5f49a99ac075f1a471b78 🚀

---
+++ assets/FileInput-5d4d35cc.js
@@ -0,0 +1,1 @@
+import{r,j as s}from"./index-1dbf5670.js";const o=r.forwardRef(({handleChangeFileInput:t},e)=>s.jsx("input",{style:{display:"none"},type:"file",multiple:!0,onChange:t,ref:e}));export{o as F};
---
+++ assets/convertTimeWithZero-0e4ead24.js
@@ -0,0 +1,1 @@
+const e=t=>{const o=Math.floor(t/60),n=Math.floor(t%60/10),r=Math.floor(t%10);return`${o}:${n}${r}`};export{e as c};
