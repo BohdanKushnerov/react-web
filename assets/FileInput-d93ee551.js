92d9f3c27e29d2dc1e333c188740a499df80bced
BohdanKushnerov <bohdan.kushnerov@gmail.com>
2024-06-27 14:21:44 +0000

    Deploying to gh-pages from @ BohdanKushnerov/react-web@39d5ef8ce34765f6a53ce4ff7e8c4b610ac35366 🚀

--- assets/FileInput-d93ee551.js
+++
@@ -1,1 +0,0 @@
-import{r,j as s}from"./index-fe10a68b.js";const o=r.forwardRef(({handleChangeFileInput:t},e)=>s.jsx("input",{style:{display:"none"},type:"file",multiple:!0,onChange:t,ref:e}));export{o as F};
--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,14 +2,14 @@ importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js');
 
 const firebaseConfig = {
-  apiKey: 'process.env.VITE_API_KEY',
-  authDomain: 'process.env.VITE_AUTH_DOMAIN',
-  projectId: 'process.env.VITE_PROJECT_ID',
-  storageBucket: 'process.env.VITE_STORAGE_BUCKET',
-  messagingSenderId: 'process.env.VITE_MESSAGING_SENDER_ID',
-  appId: 'process.env.VITE_APP_ID',
-  measurementId: 'process.env.VITE_MEASUREMENT_ID',
-  databaseURL: 'process.env.VITE_DB_URL',
+  apiKey: '1111111111111',
+  authDomain: '22222222222',
+  projectId: '33333333333333',
+  storageBucket: '4444444444444444444',
+  messagingSenderId: '555555555555555555555',
+  appId: '666666666666666666666',
+  measurementId: '77777777777777777777777777',
+  databaseURL: '8888888888888888888888',
 };
 
 firebase.initializeApp(firebaseConfig);
